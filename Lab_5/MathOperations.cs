d598b56e787c2134111e48eae38b81cd48c824db
Nikita <nikitasergeenko2001@mail.ru>
2020-12-14 12:00:14 +0300

    Добавьте файлы проекта.

---
+++ Lab_5/MathOperations.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Lab_5
+{
+    class MathOperations
+    {
+        public static int FindNumbersSum(int number)
+        {
+            int sum = 0;
+            while (number >= 1)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+
+        public static bool IsOneDigitEven(int number)
+        {
+            bool result = false;
+            while (number >= 1)
+            {
+                if (number % 10 % 2 == 0)
+                {
+                    result = true;
+                    break;
+                }
+                number /= 10;
+            }
+            return result;
+        } 
+
+        public static bool IsPalindrom(int number)
+        {
+            if (number > 0 && number < 10)
+            {
+                return true;
+            }
+            int numberLeft = number;
+            int numberRight = 0;
+            while (number >= 1)
+            {
+                numberRight *= 10;
+                numberRight += number % 10;
+                number /= 10;
+            }
+            if (numberLeft == numberRight)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int FindMaxDigit(int number)
+        {
+            int maxDigit = 0;
+            while (number >= 1)
+            {
+                if (maxDigit < number % 10)
+                {
+                    maxDigit = number % 10;
+                }
+                number /= 10;
+            }
+            return maxDigit;
+        }
+
+        public static int FindFibonacciNumber(int position)
+        {
+            if (position == 1)
+            {
+                return 0;
+            }
+            else if (position == 2)
+            {
+                return 1;
+            }
+            return FindFibonacciNumber(position - 1) + FindFibonacciNumber(position - 2);
+        }
+    }
+}
---
+++ Lab_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a natural number:");
+            number = CheckInput();
+            Console.WriteLine($"The sum of digits in number {number} is {MathOperations.FindNumbersSum(number)}");
+
+            string msg = MathOperations.IsOneDigitEven(number) == true ? "has at least one even digit" : "doesn't have even digits";
+            Console.WriteLine($"The number {number} {msg}");
+
+            string isPalindrom = MathOperations.IsPalindrom(number) == true ? "is" : "isn't";
+            Console.WriteLine($"The number {number} {isPalindrom} palindrom");
+
+            Console.WriteLine($"Maximal digit in number {number} is {MathOperations.FindMaxDigit(number)}");
+
+            int position;
+            Console.WriteLine("Enter a position of Fibonacci number:");
+            position = CheckInput();
+
+            Console.WriteLine($"The {position} number in Fibonacci sequence is {MathOperations.FindFibonacciNumber(position)}");
+        }
+
+        static int CheckInput()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < 1)
+            {
+                Console.WriteLine("You enter invalid data!");
+            }
+            return number;
+        }
+    }
+}
